1144147d4ae6f1ea3e0721801138b2957ed77b5f
cpdms2 <136682290+cpdms2@users.noreply.github.com>
2024-11-16 18:22:45 +0530

    code commit

---
+++ VardaanCab.APP/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VardaanCab.DataAccessLayer.DataLayer;
+using VardaanCab.Domain.DTOAPI;
+namespace VardaanCab.APP.Controllers
+{
+    public class AccountController : ApiController
+    {
+        Vardaan_AdminEntities ent = new Vardaan_AdminEntities();
+        [HttpPost]
+        public IHttpActionResult LoginMaster(MasterLoginDTO model)
+        {
+            try
+            {
+                var CustomerResult = ent.Customers.Where(x => x.ContactNo == model.UserName && x.IsActive == true).FirstOrDefault();
+                var DriverResult = ent.Drivers.Where(x => x.MobileNumber == model.UserName && x.IsActive == true).FirstOrDefault();
+                if(CustomerResult.IsActive == true && CustomerResult != null)
+                {
+                    if("IsOtp" == "True")
+                    {
+                        return Ok(new { Status = 200, Message = "Please Enter the OTP...!", Data = CustomerResult });
+                    }
+                    else
+                    {
+                        return Ok(new { Status = 200, Message = "Please Enter the Password...!", Data = CustomerResult });
+                    }
+                }
+                else if(DriverResult.IsActive == true && DriverResult != null)
+                {
+                    if ("IsOtp" == "True")
+                    {
+                        return Ok(new { Status = 200, Message = "Please Enter the OTP...!", Data = CustomerResult });
+                    }
+                    else
+                    {
+                        return Ok(new { Status = 200, Message = "Please Enter the Password...!", Data = CustomerResult });
+                    }
+
+                }
+                else
+                {
+                    return Ok(new { Status = HttpStatusCode.NonAuthoritativeInformation, Message = "You Are not a Authrized Person...!" });
+                } 
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Server Error : " + ex.Message);
+            }
+        }
+    }
+}
---
+++ VardaanCab.Domain/DTOAPI/MasterLoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VardaanCab.Domain.DTOAPI
+{
+    public class MasterLoginDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
