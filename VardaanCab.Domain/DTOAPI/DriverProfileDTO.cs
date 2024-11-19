082bed0b903c67fc1c88e277964a44699703dcec
mmm <139609691+rajputmadhu108@users.noreply.github.com>
2024-11-19 16:06:10 +0530

    okkkk

---
+++ VardaanCab.Domain/DTOAPI/DriverProfileDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VardaanCab.Domain.DTOAPI
+{
+    public class DriverProfileDTO
+    {
+        public int Id { get; set; }
+        public System.DateTime CreateDate { get; set; }
+        public string DriverName { get; set; }
+        public string DlNumber { get; set; }
+        public string DlImage { get; set; }
+        public string AadharNo { get; set; }
+        public string AadharImage { get; set; }
+        public string DriverImage { get; set; } 
+        public string MobileNumber { get; set; } 
+        public string DriverAddress { get; set; } 
+        public string PanImage { get; set; }
+        public string PanNumber { get; set; } 
+    }
+}
