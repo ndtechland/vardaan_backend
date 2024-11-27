313e95cd02dbdca072acd57cf893e43e5c9163f4
ndshubham <153910931+ndshubham@users.noreply.github.com>
2024-11-27 13:10:40 +0530

    code commit

--- VardaanCab.APP/Controllers/AccountController.cs
+++ VardaanCab.APP/Controllers/AccountController.cs
@@ -396,7 +396,7 @@ namespace VardaanCab.APP.Controllers
                         else
                         {
                             return BadRequest("Confirm Password not matched.");
-                        } 
+                        }  
                 }
                 else
                 {
