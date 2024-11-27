ccc7e5a4df3aff91ea6a7ef1ecc4c9a412184310
mmm <139609691+rajputmadhu108@users.noreply.github.com>
2024-11-27 16:33:23 +0530

    Etsokk

---
+++ VardaanCab.Domain/DTO/CreateRequestDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VardaanCab.Domain.DTO
+{
+    public class CreateRequestDTO
+    {
+        public string RequestType { get; set; }
+        public string EmployeeId { get; set; } 
+        public string Gender { get; set; } 
+        public string Email { get; set; } 
+        public string Unit { get; set; } 
+        public string Department { get; set; } 
+        public string RequestorEmployeeId { get; set; } 
+        public string RequestorName { get; set; } 
+        public DateTime? BookingDate { get; set; }
+        public DateTime? StartRequestDate { get; set; }
+        public DateTime? EndRequestDate { get; set; }
+        public string TripType { get; set; }
+        public string LocationType { get; set; }
+        public string ShiftType { get; set; }
+    }
+}
---
+++ VardaanCab/Controllers/ETSController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VardaanCab.DataAccessLayer.DataLayer;
+
+namespace VardaanCab.Controllers
+{
+    public class ETSController : Controller
+    {
+        Vardaan_AdminEntities ent = new Vardaan_AdminEntities();
+        // GET: ETS
+        public ActionResult CreateRequest()
+        {
+            return View();
+        }
+        public JsonResult GetEmployeeDetails(string id)
+        {
+            var employee = ent.Employees.FirstOrDefault(e => e.Employee_Id == id);
+
+            if (employee != null)
+            {
+                var employeeData = new
+                {
+                    firstName = employee.Employee_First_Name,
+                    lastName = employee.Employee_Last_Name, 
+                    gender = employee.Gender,
+                    email = employee.Email,
+                    guestContactNumber = employee.MobileNumber
+                };
+
+                return Json(employeeData,JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(null); // Return null if employee is not found
+            }
+        }
+
+    }
+}
