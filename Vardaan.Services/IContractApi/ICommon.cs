6738a721806411dbd4ef3595b33d659635408ca1
cpdms2 <136682290+cpdms2@users.noreply.github.com>
2024-12-17 12:32:44 +0530

    code commit

---
+++ Vardaan.Services/IContractApi/ICommon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VardaanCab.DataAccessLayer.DataLayer;
+namespace Vardaan.Services.IContract
+{
+    public interface ICommon
+    {
+      Task<List<BannerMaster>> GetBanner(string role);
+    }
+}
--- VardaanCab/Global.asax.cs
+++ VardaanCab/Global.asax.cs
@@ -18,6 +18,7 @@ namespace VardaanCab
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
+            UnityMvcActivator.Start();
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
