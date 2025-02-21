12ab11a347dcb53038809aebe6ad61b9977f47c8
mmm <139609691+rajputmadhu108@users.noreply.github.com>
2025-02-21 16:44:04 +0530

    fshiufhus

---
+++ VardaanCab.Domain/DTOAPI/RoutingDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VardaanCab.Domain.DTOAPI
+{
+    public class RoutingDTO
+    {
+        public int RoutId { get; set; }
+        public string VehicleNumber { get; set; }
+        public string Location { get; set; }
+        public int CabId { get; set; }
+    }
+}
